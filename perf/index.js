6fc5cb94667d3ddc29db9168766cd5b332bed8ff
Jacopo Scazzosi <me@jacoscaz.com>
2017-03-17 17:04:46 +0000

    fixes perf test

--- perf/index.js
+++ perf/index.js
@@ -126,7 +126,7 @@ async.series(
       const quads = new Array(times).fill(true).map(() => generateQuad(rdflibFactory));
       time(
         'rdf   / import / rdflib',
-        (done) => { store.import(new AsyncIterator.ArrayIterator(quads), done); },
+        (done) => { store.import(new AsyncIterator.ArrayIterator(quads)).on('end', done); },
         (err, result) => { removeStore(store); cb(err, result); }
       )
     },
@@ -136,7 +136,7 @@ async.series(
       const quads = new Array(times).fill(true).map(() => generateQuad(rdfextFactory));
       time(
         'rdf   / import / rdfext',
-        (done) => { store.import(new AsyncIterator.ArrayIterator(quads), done); },
+        (done) => { store.import(new AsyncIterator.ArrayIterator(quads)).on('end', done); },
         (err, result) => { removeStore(store); cb(err, result); }
       )
     }

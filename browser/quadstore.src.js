ef033f41dcf6e8e96310cfcad6d061104d69088b
Jacopo Scazzosi <me@jacoscaz.com>
2018-09-07 22:58:00 +0100

    deletes browserify-related directory

--- browser/quadstore.src.js
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-
-const leveljs = require('level-js');
-const quadstore = require('..');
-
-module.exports.leveljs = leveljs;
-module.exports.RdfStore = quadstore.RdfStore;
-module.exports.QuadStore = quadstore.QuadStore;

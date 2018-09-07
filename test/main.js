edd027814eb691eff6de63f5c3fc04f036f61704
Jacopo Scazzosi <me@jacoscaz.com>
2018-09-07 22:23:04 +0100

    updates level* dependencies to fix compat issues with node 10.x

--- test/main.js
+++ test/main.js
@@ -6,9 +6,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const shortid = require('shortid');
 const memdown = require('memdown');
-const levelup = require('levelup');
 const Promise = require('bluebird');
-const QuadStore = require('..').QuadStore;
 const leveldown = require('leveldown');
 const rdfStoreSuite = require('./rdfstore');
 const quadStoreSuite = require('./quadstore');
@@ -18,8 +16,7 @@ const remove = Promise.promisify(fs.remove, { context: fs });
 describe('MemDOWN backend', () => {
 
   beforeEach(async function () {
-    this.location = shortid();
-    this.db = levelup(this.location, { valueEncoding: QuadStore.valueEncoding, db: memdown });
+    this.db = memdown();
   });
 
   quadStoreSuite();
@@ -31,7 +28,7 @@ describe('LevelDOWN backend', () => {
 
   beforeEach(async function () {
     this.location = path.join(os.tmpdir(), 'node-quadstore-' + shortid.generate());
-    this.db = levelup(this.location, { valueEncoding: QuadStore.valueEncoding, db: leveldown });
+    this.db = leveldown(this.location);
   });
 
   afterEach(async function () {

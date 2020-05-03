762f4f8ce902c6c62a1b2edcb036b60dfc58c2e7
Jacopo Scazzosi <me@jacoscaz.com>
2020-05-04 00:16:59 +0200

    adds basic support for SPARQL INSERT DATA and DELETE DATA

--- test/rdfstore.prototype.sparql.js
+++ test/rdfstore.prototype.sparql.js
@@ -10,48 +10,94 @@ module.exports = () => {
 
   describe('RdfStore.prototype.sparql()', () => {
 
-    beforeEach(async function () {
-      const quads = [
-        factory.quad(
-          factory.namedNode('http://ex.com/s'),
-          factory.namedNode('http://ex.com/p'),
-          factory.namedNode('http://ex.com/o'),
-          factory.namedNode('http://ex.com/g')
-        ),
-        factory.quad(
-          factory.namedNode('http://ex.com/s'),
-          factory.namedNode('http://ex.com/p2'),
-          factory.namedNode('http://ex.com/o2'),
-          factory.namedNode('http://ex.com/g2')
-        ),
-        factory.quad(
-          factory.namedNode('http://ex.com/s2'),
-          factory.namedNode('http://ex.com/p'),
-          factory.namedNode('http://ex.com/o'),
-          factory.namedNode('http://ex.com/g')
-        ),
-        factory.quad(
-          factory.namedNode('http://ex.com/s2'),
-          factory.namedNode('http://ex.com/p'),
-          factory.namedNode('http://ex.com/o2'),
-          factory.namedNode('http://ex.com/g')
-        ),
-        factory.quad(
-          factory.namedNode('http://ex.com/s2'),
-          factory.namedNode('http://ex.com/p2'),
-          factory.namedNode('http://ex.com/o2'),
-          factory.namedNode('http://ex.com/g2')
-        ),
-      ];
-      await this.store.put(quads);
+    describe('SELECT', () => {
+
+      beforeEach(async function () {
+        const quads = [
+          factory.quad(
+            factory.namedNode('http://ex.com/s'),
+            factory.namedNode('http://ex.com/p'),
+            factory.namedNode('http://ex.com/o'),
+            factory.namedNode('http://ex.com/g')
+          ),
+          factory.quad(
+            factory.namedNode('http://ex.com/s'),
+            factory.namedNode('http://ex.com/p2'),
+            factory.namedNode('http://ex.com/o2'),
+            factory.namedNode('http://ex.com/g2')
+          ),
+          factory.quad(
+            factory.namedNode('http://ex.com/s2'),
+            factory.namedNode('http://ex.com/p'),
+            factory.namedNode('http://ex.com/o'),
+            factory.namedNode('http://ex.com/g')
+          ),
+          factory.quad(
+            factory.namedNode('http://ex.com/s2'),
+            factory.namedNode('http://ex.com/p'),
+            factory.namedNode('http://ex.com/o2'),
+            factory.namedNode('http://ex.com/g')
+          ),
+          factory.quad(
+            factory.namedNode('http://ex.com/s2'),
+            factory.namedNode('http://ex.com/p2'),
+            factory.namedNode('http://ex.com/o2'),
+            factory.namedNode('http://ex.com/g2')
+          ),
+        ];
+        await this.store.put(quads);
+      });
+
+    });
+    // it('should do stuff', async function () {
+    //   const iterator = this.store.sparqlStream(`
+    //     SELECT * { ?s <http://ex.com/p> <http://ex.com/o>. }
+    //   `);
+    //   const results = await utils.streamToArray(iterator);
+    //   console.log(results);
+    // });
+    //
+    // it('should do stuff 2', async function () {
+    //   const iterator = this.store.sparqlStream(`
+    //     SELECT ?s ?o {
+    //       ?s <http://ex.com/p> <http://ex.com/o>.
+    //       ?s <http://ex.com/p2> ?o.
+    //     }
+    //   `);
+    //   const results = await utils.streamToArray(iterator);
+    //   console.log(results);
+    // });
+
+    describe('INSERT DATA', () => {
+
+      it('should insert a single quad', async function () {
+        await this.store.sparql(`
+          INSERT DATA { GRAPH <ex://g3> { <ex://s3> <ex://p3> <ex://o3>. } . <ex://s4> <ex://p4> <ex://o4> . } 
+        `);
+        const quads = await this.store.get({ graph: factory.namedNode('ex://g3') });
+        should(quads.length).equal(1);
+      });
+
     });
 
-    it('should do stuff', async function () {
-      const iterator = this.store.sparqlStream(`
-        SELECT * { ?s <http://ex.com/p> <http://ex.com/o>. }
-      `);
-      const results = await utils.streamToArray(iterator);
-      console.log(results);
+    describe('DELETE DATA', () => {
+
+      it('should insert a single quad', async function () {
+        await this.store.sparql(`
+          INSERT DATA { 
+            GRAPH <ex://g3> { <ex://s3> <ex://p3> <ex://o3>. } . 
+            GRAPH <ex://g4> { <ex://s4> <ex://p4> <ex://o4>. } .
+          } 
+        `);
+        should(await this.store.get({})).have.length(2);
+        await this.store.sparql(`
+          DELETE DATA { 
+            GRAPH <ex://g3> { <ex://s3> <ex://p3> <ex://o3>. } .
+          } 
+        `);
+        should(await this.store.get({})).have.length(1);
+      });
+
     });
 
 

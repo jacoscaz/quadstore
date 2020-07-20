451e0437debf7394d25afe2828f514a7035a3672
Jacopo Scazzosi <me@jacoscaz.com>
2020-07-20 17:52:23 +0200

    SPARQL: adds a couple more tests for INSERT DATA queries

--- test/sparql/sparql.update.insertdata.js
+++ test/sparql/sparql.update.insertdata.js
@@ -4,14 +4,70 @@ const should = require('should');
 const factory = require('@rdfjs/data-model');
 
 module.exports = () => {
-  describe('INSERT DATA', () => {
+  describe.only('INSERT DATA', () => {
 
-    it('should insert a single quad', async function () {
+    it('should insert a single quad in the default graph', async function () {
       await this.store.sparql(`
-        INSERT DATA { GRAPH <ex://g3> { <ex://s3> <ex://p3> <ex://o3>. } . <ex://s4> <ex://p4> <ex://o4> . } 
+        INSERT DATA { <ex://s4> <ex://p4> <ex://o4> . } 
       `);
-      const { items } = await this.store.get({graph: factory.namedNode('ex://g3')});
+      const { items } = await this.store.get({});
       should(items.length).equal(1);
+      should(items[0].subject.equals(factory.namedNode('ex://s4')));
+      should(items[0].predicate.equals(factory.namedNode('ex://p4')));
+      should(items[0].object.equals(factory.namedNode('ex://o4')));
+      should(items[0].graph.equals(factory.defaultGraph()));
+    });
+
+    it('should insert a single quad in a custom graph', async function () {
+      await this.store.sparql(`
+        INSERT DATA { GRAPH <ex://g4> { <ex://s4> <ex://p4> <ex://o4> . } . } 
+      `);
+      const { items } = await this.store.get({});
+      should(items.length).equal(1);
+      should(items[0].subject.equals(factory.namedNode('ex://s4')));
+      should(items[0].predicate.equals(factory.namedNode('ex://p4')));
+      should(items[0].object.equals(factory.namedNode('ex://o4')));
+      should(items[0].graph.equals(factory.namedNode('ex://g4')));
+    });
+
+    it('should insert multiple quads in the default graph', async function () {
+      await this.store.sparql(`
+        INSERT DATA {
+          <ex://s3> <ex://p3> <ex://o3> .
+          <ex://s4> <ex://p4> <ex://o4> . 
+        }  
+      `);
+      const { items } = await this.store.get({});
+      should(items.length).equal(2);
+      should(items[0].subject.equals(factory.namedNode('ex://s3')));
+      should(items[0].predicate.equals(factory.namedNode('ex://p3')));
+      should(items[0].object.equals(factory.namedNode('ex://o3')));
+      should(items[0].graph.equals(factory.defaultGraph()));
+      should(items[1].subject.equals(factory.namedNode('ex://s4')));
+      should(items[1].predicate.equals(factory.namedNode('ex://p4')));
+      should(items[1].object.equals(factory.namedNode('ex://o4')));
+      should(items[1].graph.equals(factory.defaultGraph()));
+    });
+
+    it('should insert multiple quads in a custom graph', async function () {
+      await this.store.sparql(`
+        INSERT DATA {
+          GRAPH <ex://g3> { 
+            <ex://s3> <ex://p3> <ex://o3> .
+            <ex://s4> <ex://p4> <ex://o4> .
+          } . 
+        }  
+      `);
+      const { items } = await this.store.get({});
+      should(items.length).equal(2);
+      should(items[0].subject.equals(factory.namedNode('ex://s3')));
+      should(items[0].predicate.equals(factory.namedNode('ex://p3')));
+      should(items[0].object.equals(factory.namedNode('ex://o3')));
+      should(items[0].graph.equals(factory.namedNode('ex://g3')));
+      should(items[1].subject.equals(factory.namedNode('ex://s4')));
+      should(items[1].predicate.equals(factory.namedNode('ex://p4')));
+      should(items[1].object.equals(factory.namedNode('ex://o4')));
+      should(items[1].graph.equals(factory.namedNode('ex://g3')));
     });
 
   });

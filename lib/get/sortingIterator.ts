9ed09edc1059c18f74b401411524165105783a2b
Jacopo Scazzosi <jacopo@scazzosi.com>
2022-03-24 15:17:29 +0100

    makes SortingIterator faster by extending AsyncIterator in place of BufferedIterator

--- lib/get/sortingIterator.ts
+++ lib/get/sortingIterator.ts
@@ -1,6 +1,5 @@
 
-import type { AsyncIterator } from 'asynciterator';
-import {BufferedIterator, TransformIterator} from 'asynciterator';
+import { AsyncIterator } from 'asynciterator';
 
 const SortedSet = require('js-sorted-set');
 
@@ -8,10 +7,7 @@ const SortedSet = require('js-sorted-set');
  * Buffers all items emitted from `source` and sorts them according to
  * `compare`.
  */
-export class SortingIterator<In, Int, Out> extends BufferedIterator<Out> {
-
-  private publish: (item: Int) => Out;
-  private iterator?: any;
+export class SortingIterator<In, Int, Out> extends AsyncIterator<Out> {
 
   /**
    *
@@ -19,54 +15,47 @@ export class SortingIterator<In, Int, Out> extends BufferedIterator<Out> {
    * @param compare
    * @param digest
    * @param emit
-   * @param options
    */
   public constructor(
     source: AsyncIterator<In>,
     compare: (left: Int, right: Int) => number,
     digest: (item: In) => Int,
     emit: (item: Int) => Out,
-    options?: any,
   ) {
 
-    super(options);
+    super();
 
-    this.publish = emit;
+    let iterator: any;
 
-    this._read = (count: number, done: () => void): void => {
+    const startBuffering = () => {
       const set = new SortedSet({ comparator: compare });
       const onData = (item: In) => {
         set.insert(digest(item));
       };
       const onEnd = () => {
         source.removeListener('data', onData);
-        this.iterator = set.beginIterator();
-        // @ts-ignore
-        delete this._read;
-        this._read(count, done);
+        source.removeListener('end', onEnd);
+        iterator = set.beginIterator();
+        this.emit('readable');
       };
       source.on('data', onData);
       source.once('end', onEnd);
     };
 
-  }
-
-  public _read(count: number, done: () => void): void {
-    let iterator = this.iterator;
-    let publish = this.publish!;
-    let value;
-    while (count >= 0) {
-      if ((value = iterator.value()) === null) {
-        done();
+    this.read = (): Out | null => {
+      const value = iterator.value();
+      if (value === null) {
         this.close();
-        return;
+        return null;
       }
-      this._push(publish(value));
       iterator = iterator.next();
-      count -= 1;
-    }
-    this.iterator = iterator;
-    done();
+      return emit(value);
+    };
+
+    Promise.resolve().then(() => {
+      startBuffering();
+    });
+
   }
 
 }

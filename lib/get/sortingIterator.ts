5b3e4c754e4bce5bb88b62418b51a9c97b87e8d1
Jacopo Scazzosi <jacopo@scazzosi.com>
2022-05-24 14:13:06 +0200

    improves error and readable status handling in sorting iterator

--- lib/get/sortingIterator.ts
+++ lib/get/sortingIterator.ts
@@ -32,24 +32,32 @@ export class SortingIterator<In, Int, Out> extends AsyncIterator<Out> {
       const onData = (item: In) => {
         set.insert(digest(item));
       };
+      const onError = (err: Error) => {
+        this.emit('error', err);
+      };
       const onEnd = () => {
         source.removeListener('data', onData);
+        source.removeListener('error', onError);
         source.removeListener('end', onEnd);
         iterator = set.beginIterator();
-        this.emit('readable');
+        this.readable = true;
       };
       source.on('data', onData);
+      source.on('error', onError);
       source.once('end', onEnd);
     };
 
     this.read = (): Out | null => {
-      const value = iterator.value();
-      if (value === null) {
-        this.close();
-        return null;
+      if (iterator) {
+        const value = iterator.value();
+        if (value === null) {
+          this.close();
+          return null;
+        }
+        iterator = iterator.next();
+        return emit(value);
       }
-      iterator = iterator.next();
-      return emit(value);
+      return null;
     };
 
     Promise.resolve().then(() => {

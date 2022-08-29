9fc820435ec82ca177d07b112914c2d55f02c39a
Jacopo Scazzosi <jacopo@scazzosi.com>
2022-08-29 10:17:20 +0200

    adds script to automatically fix import/export paths generated from TS when compiling to ES modules

--- fix-esm-import-paths.js
+++ fix-esm-import-paths.js
@@ -1,22 +1,37 @@
 
-// Script largely inspired by https://gist.github.com/silassare/cc3db860edeea07298cb03f64738c997
+// Script largely inspired by:
+// - https://gist.github.com/silassare/cc3db860edeea07298cb03f64738c997
+// - https://github.com/dsblv/string-replace-async/blob/7a7b1c6c5db728c298ac12df05c4eaf7e0ec8805/index.js
 
 const fs = require('fs');
 const path = require('path');
 
 const SRC_DIR = path.resolve(__dirname, 'dist', 'esm');
 
+const { readFile, writeFile, stat, opendir } = fs.promises;
+
 const fileExists = async (filePath) => {
   try {
-    const stat = await fs.promises.stat(filePath);
-    return stat.isFile();
+    return (await stat(filePath)).isFile();
   } catch (err) {
     return false;
   }
 };
 
+const stringReplace = async (string, searchValue, replacer) => {
+  const promises = [];
+  String.prototype.replace.call(string, searchValue, function () {
+    promises.push(replacer.apply(undefined, arguments));
+    return '';
+  });
+  const values = await Promise.all(promises);
+  return String.prototype.replace.call(string, searchValue, function () {
+    return values.shift();
+  });
+};
+
 const walk = async function* (dir) {
-  for await (const entryStat of await fs.promises.opendir(dir)) {
+  for await (const entryStat of await opendir(dir)) {
     const entry = path.resolve(dir, entryStat.name);
     if (entryStat.isDirectory()) {
       yield* walk(entry);
@@ -27,9 +42,8 @@ const walk = async function* (dir) {
 };
 
 const resolveImportPath = async (sourceFile, importPath) => {
-  const sourceFileAbs = path.resolve(process.cwd(), sourceFile);
-  const root = path.dirname(sourceFileAbs);
-  if (!path.isAbsolute(importPath) && !importPath.startsWith('@') && !importPath.endsWith('.js')) {
+  const root = path.dirname(sourceFile);
+  if (importPath.match(/^\.\.?\//)) {
     const importPathAbs = path.resolve(root, importPath);
     let possiblePath = [
       path.resolve(importPathAbs, './index.js'),
@@ -52,27 +66,25 @@ const resolveImportPath = async (sourceFile, importPath) => {
 };
 
 const replace = async (filePath, outFilePath) => {
-  const code = await fs.promises.readFile(filePath, 'utf8');
-  const newCode = code.replace(
-    /(import) (.+?) from ('[^\n']+'|"[^\n"]+");/gs,
-    function (found, action, imported, from) {
+  const code = await readFile(filePath, 'utf8');
+  const newCode = await stringReplace(code,
+    /(import|export) (.+?) from ('[^\n']+'|"[^\n"]+");/gs,
+    async (found, action, imported, from) => {
       const importPath = from.slice(1, -1);
-      const resolvedPath = resolveImportPath(filePath, importPath);
+      const resolvedPath = await resolveImportPath(filePath, importPath);
       if (resolvedPath) {
-        console.log('\t', importPath, resolvedPath);
         return `${action} ${imported} from '${resolvedPath}';`;
       }
       return found;
     });
   if (code !== newCode) {
-    await fs.promises.writeFile(outFilePath, newCode);
+    await writeFile(outFilePath, newCode);
   }
 };
 
 const main = async () => {
   for await (const entry of walk(SRC_DIR)) {
     if (path.extname(entry) === '.js') {
-      console.log(entry);
       await replace(entry, entry);
     }
   }

8e06ac89ba9ef2f452728816efc41b04e068459a
Jacopo Scazzosi <jacopo@scazzosi.com>
2023-03-22 14:02:11 +0100

    uses radix 36 to parse / serialize lengths to support longer terms

--- src/serialization/utils.ts
+++ src/serialization/utils.ts
@@ -4,9 +4,9 @@ export const sliceString = (source: string, offset: number, length: number): str
 };
 
 export const padNumStart = (val: number) => {
-  if (val < 10) return '000' + val;
-  if (val < 100) return '00' + val;
-  if (val < 1000) return '0' + val;
-  if (val < 10000) return '' + val;
+  if (val < 36) return `000${val.toString(36)}`;
+  if (val < 1_296) return `00${val.toString(36)}`;
+  if (val < 46_656) return `0${val.toString(36)}`;
+  if (val < 1_679_615) return val.toString(36);
   throw new Error('too long: ' + val);
 };

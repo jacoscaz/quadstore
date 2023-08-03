473ac4bcd821a520dfb72ffd94e447d3eaa232dc
Jacopo Scazzosi <jacopo@scazzosi.com>
2023-08-03 11:03:06 +0200

    updates deps

--- test/browser.ts
+++ test/browser.ts
@@ -19,7 +19,7 @@ let exitCode = 0;
 
 httpServer.once('listening', () => {
   (async () => {
-    const browser = await puppeteer.launch({});
+    const browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
     page.on('console', async (evt) => {
       const [msg, ...args] = await Promise.all(evt.args().map(arg => arg.jsonValue()));
